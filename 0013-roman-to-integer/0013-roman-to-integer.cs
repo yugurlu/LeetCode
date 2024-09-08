b7f9a3f254607d3705b37a2789c602d28f6934b4
Yusuf Ugurlu <108735272+yugurlu@users.noreply.github.com>
2024-09-08 17:08:27 +0300

    Time: 65 ms (11.19%), Space: 50.4 MB (15.45%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cs
+++ 0013-roman-to-integer/0013-roman-to-integer.cs
@@ -13,12 +13,12 @@ public class Solution {
             new KeyValuePair<char, int>('M', 1000),
         };
 
-        if (s.Contains("IV")) s = s.Replace("IV", "IIII");
-        if (s.Contains("IX")) s = s.Replace("IX", "VIIII");
-        if (s.Contains("XL")) s = s.Replace("XL", "XXXX");
-        if (s.Contains("XC")) s = s.Replace("XC", "LXXXX");
-        if (s.Contains("CD")) s = s.Replace("CD", "CCCC");
-        if (s.Contains("CM")) s = s.Replace("CM", "DCCCC");
+        s = s.Replace("IV", "IIII");
+        s = s.Replace("IX", "VIIII");
+        s = s.Replace("XL", "XXXX");
+        s = s.Replace("XC", "LXXXX");
+        s = s.Replace("CD", "CCCC");
+        s = s.Replace("CM", "DCCCC");
 
         for (int i = 0; i < s.Length; i++)
             res += list.FirstOrDefault(x => x.Key == s[i]).Value;
