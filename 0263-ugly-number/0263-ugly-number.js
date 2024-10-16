ab5d00729cf1d24f3bb2545df2106ad1cd208f16
Yusuf Ugurlu <108735272+yugurlu@users.noreply.github.com>
2024-10-16 12:32:29 +0300

    Time: 74 ms (42.47%), Space: 51.9 MB (85.65%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function (n) {
+    if (n <= 0) return false;
+    if (n == 1) return true;
+
+    while (n != 1) {
+        if (n % 5 === 0) n /= 5;
+        else if (n % 3 === 0) n /= 3;
+        else if (n % 2 === 0) n /= 2;
+        else return false;
+    }
+
+    return true;
+};
