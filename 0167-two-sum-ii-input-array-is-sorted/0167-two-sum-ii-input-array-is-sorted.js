933e95eeb3653cafe8148d80259b1d02c26263f0
Yusuf Ugurlu <108735272+yugurlu@users.noreply.github.com>
2024-10-17 14:47:09 +0300

    Time: 3 ms (100%), Space: 50.8 MB (7.25%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+    let i = 0;
+    let j = numbers.length - 1;
+
+    while (i < j) {
+        if (numbers[i] + numbers[j] === target) {
+            return [++i, ++j];
+        }
+
+        if (numbers[i] + numbers[j] > target) {
+            j--;
+        }
+        else {
+            i++;
+        }
+    }
+};
